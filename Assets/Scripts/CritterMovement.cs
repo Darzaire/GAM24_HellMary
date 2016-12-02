9c82927cd56f8c8259681758d1674832b4991ec9
Unknown <Jkonechy@live.com>
2016-12-02 15:39:26 -0800

    Added Enemies and critter prefabs/scripts

---
+++ Assets/Scripts/CritterMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CritterMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    float walkTime;
+
+    void NewDirection()
+    {
+        float temp = Random.Range(0f, 360f);
+        transform.eulerAngles = new Vector3(0, temp, 0);
+
+        walkTime = Random.Range(5f, 10f);
+    }
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (walkTime > 0)
+        {
+            walkTime -= Time.deltaTime;
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public Transform target;
+    public Transform myTransform;
+    void Start ()
+    {
+       
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTransform.LookAt(target);
+        myTransform.Translate(Vector3.forward * 5 * Time.deltaTime);
+	}
+
+   
+
+}
