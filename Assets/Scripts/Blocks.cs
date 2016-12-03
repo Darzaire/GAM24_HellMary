5079f684875d33ed21bf178ee4e001bd58233bf0
Unknown <deadstrife@hotmail.com>
2016-12-02 16:15:09 -0800

    Blocks
     and stuff

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocks : MonoBehaviour {
+
+    public enum BlockTypes { Grass, Stone , Sand, Gravel, Water, Coal, Iron, Gold, Diamond /*,Cloud*/  };
+
+    public Material[] blockmats;
+
+    public BlockTypes thisBlockType;
+    int health;
+    // Use this for initialization
+    void Start () {
+        health = 2;
+
+        thisBlockType = BlockTypes.Sand;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
