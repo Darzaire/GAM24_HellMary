5b59417a6dc497f79f58cfcfc92a7d4de3a619ea
Unknown <Jkonechy@live.com>
2016-12-02 16:08:01 -0800

    WhenSuddenly it Changes
    Violently it changes!!

--- Assets/Scripts/CritterMovement.cs
+++ Assets/Scripts/CritterMovement.cs
@@ -4,28 +4,36 @@ using System.Collections;
 public class CritterMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    float walkTime;
+    public float walkTime = 5.0f;
+    public float WalkCoolDown = 3.0f;
+
 
     void NewDirection()
     {
         float temp = Random.Range(0f, 360f);
         transform.eulerAngles = new Vector3(0, temp, 0);
 
-        walkTime = Random.Range(5f, 10f);
+        walkTime = Random.Range(9f, 10f);
     }
 	// Use this for initialization
 	void Start ()
     {
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (walkTime > 0)
         {
             walkTime -= Time.deltaTime;
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        }
+
+        else
+        {
+            NewDirection();
         }
 	}
 }
