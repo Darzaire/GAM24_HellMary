d17e4182b26bddcb050e0115c8a8358f3df46c2c
DemoneyLuffey <DmoneyLuffey@gmail.com>
2016-12-06 14:18:00 -0800

    Stuff

---
+++ Assets/Scripts/CollectTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectTest : MonoBehaviour
+{
+    public GameObject material;
+    public float playerPosition;
+    public float maxDist = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        playerPosition = transform.position.x;
+        material = GameObject.FindGameObjectWithTag("Material");
+
+        CollectMaterials();
+	}
+
+    void CollectMaterials()
+    {
+        if(playerPosition == maxDist)
+        {
+            Destroy(material);
+            print("Material collected!");
+        }
+    }
+}
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Inventory : MonoBehaviour {
+public class InventoryManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
