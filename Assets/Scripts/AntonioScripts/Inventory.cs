2e6dfe350d86ba9fa356c4b75c6c255092a66fce
tony <tonydelacruzjr@gmail.com>
2016-12-07 16:55:22 -0800

    updated the inventory script
    it works

--- Assets/Scripts/AntonioScripts/Inventory.cs
+++ Assets/Scripts/AntonioScripts/Inventory.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 public class Inventory : MonoBehaviour
 {
-    //public List<GameObject> Slots = new List<GameObject>();
-    //public List<Item> Items = new List<Item>();
+    public List<GameObject> Slots = new List<GameObject>();
+    public List<Item> Items = new List<Item>();
     public GameObject slots;
     int x = -240;
     int y = 200;
@@ -13,22 +13,22 @@ public class Inventory : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 1; i < 6; i++)
         {
-            for (int k = 0; k < 5; k++)
+            for (int k = 1; k < 6; k++)
             {
                 GameObject slot = (GameObject)Instantiate(slots);
-                //Slots.Add(slot);
-                //Items.Add(new Item());
-                slots.transform.parent = this.gameObject.transform;
+                Slots.Add(slot);
+                Items.Add(new Item());
+                slot.transform.parent = this.gameObject.transform;
                 slot.name = "slot" + i + "." + k;
                 slot.GetComponent<RectTransform>().localPosition = new Vector3(x, y, 0);
-               // x = x + 55;
-                //if(k == 5)
-               // {
-                  //  x = -200;
-                  //  y = y - 55;
-              //  }
+                x = x + 100;
+                if(k == 5)
+                {
+                    x = -240;
+                    y = y - 100;
+                }
             }
         }
 	
